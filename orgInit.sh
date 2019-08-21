9221ee56d750f633985c62d8218646afeaa85fa2
mshanemc <shane.mclaughlin@salesforce.com>
2019-08-21 16:26:50 -0500

    perms and prediction builder

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:package:install --package 04tB0000000Y9x8IAC -w 20
 sfdx force:source:push
+sfdx force:user:permset:assign -n pingpong
 sfdx force:org:open
