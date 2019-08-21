e59b79275bc65acb050e5f4d469a3820a77ca798
mshanemc <shane.mclaughlin@salesforce.com>
2019-08-21 16:56:27 -0500

    org automigration

--- orgInit.sh
+++ orgInit.sh
@@ -2,4 +2,5 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:package:install --package 04tB0000000Y9x8IAC -w 20
 sfdx force:source:push
 sfdx force:user:permset:assign -n pingpong
-sfdx force:org:open
+sfdx automig:load -d data
+sfdx force:org:open -p /lightning/o/Match__c/list?filterName=All
